7eab0db6da84af36171cc36578fd6b9174985348
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2023-01-27 08:08:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,3 @@
-/*
- * main program
- *
- *  Created on: Jan 26, 2023
- *      Author: tugrul
- */
 
 #include <string>
 #include <iostream>
@@ -11,17 +5,14 @@
 #include "Grid.h"
 
 
-
-
-
 int main(int argc, char *argv[])
 {
 
-	// x-dimension has to be multiples of 32
-	const int W=128;
+	// x-dimension to be multiples of 16 for performance
+	const int W=112;
 
 	// can be anything
-	const int H=50;
+	const int H=32;
 
 	int ch;
 
@@ -34,15 +25,29 @@ int main(int argc, char *argv[])
 
 
 
-
+	// ncurses console screen
 	Screen<W,H> screen;
+
+	// snake game grid [ compute method calculates the main game-logic ]
 	Grid<W,H> grid;
+
+	// star snake facing upwards
 	char dir=grid.UP;
+
+	// game exits when snake head collides with a snake
     bool exit = false;
+
+    // no delay for keyboard input
     nodelay(stdscr, TRUE);
+
+    // eating latch (game gives apple automatically in every second frame)
     bool eat = false;
+
+    // benchmark variables
     size_t avgTime = 0;
     int ctFrame = 0;
+
+    // game loop
 	while(true)
 	{
 		ctFrame++;
@@ -64,7 +69,6 @@ int main(int argc, char *argv[])
 			case 27:
 				exit = true;
 				break;
-
 		}
 
 		if(exit)
@@ -72,21 +76,30 @@ int main(int argc, char *argv[])
 
 		size_t nanoseconds;
 
-		// if head collides tail
+		// if head collides tail, then exit game
 		if(grid.compute( eat=!eat,dir,nanoseconds))
 		{
 			avgTime+=nanoseconds;
+
+			// ncurses finish
 			endwin();
+
+			// output performance of player and computer
 			std::cout<<"score="<<grid.len<<std::endl;
 			std::cout<<"average compute time = "<<avgTime/ctFrame<<" nanoseconds"<<std::endl;
 			return 0;
 		}
+
+		// update screen and accumulate average frame time
 		avgTime+=nanoseconds;
 		grid.updateScreen(screen);
         screen.render(nanoseconds);
 	}
 
-	endwin();			/* End curses mode		  */
+	// ncurses finish
+	endwin();
+
+	// output performance of player and computer
 	std::cout<<"score="<<grid.len<<std::endl;
 	std::cout<<"average compute time = "<<avgTime/ctFrame<<" nanoseconds"<<std::endl;
 	return 0;
